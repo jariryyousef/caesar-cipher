e6ab44e1294edb0300d24230244da9b19e60ffac
jariryyousef@gmail.com <jariryyousef@gmail.com>
2021-11-02 22:58:58 +0200

    finsh lab18

--- caesar_cipher/caesar_cipher.py
+++ caesar_cipher/caesar_cipher.py
@@ -1,5 +1,61 @@
+import re
+import nltk
+from nltk.corpus import words, names
+
+nltk.download('words', quiet=True)
+nltk.download('names', quiet=True)
+word_list = words.words()
+name_list = names.words()
 
 
-def encrypt(text,key):
-    
-    pass
+def encrypt(text, key):
+    result = "" 
+    for i in text:
+
+        if i.isupper():  
+            key_shift = ((ord(i) - ord('A')) + key) % 26 + ord('A')
+            new_letter = chr(key_shift)
+            result += new_letter
+
+        elif i.islower(): 
+            key_shift = ((ord(i) - ord('a')) + key) % 26 + ord('a')
+            new_letter = chr(key_shift)
+            result += new_letter
+        
+        elif i.isdigit():
+            result += str(((int(i) + key) % 10))
+        else:
+            result += i
+    return result
+
+def decrypt(text, key):
+
+    result = ""
+    for i in text:
+        if i.isupper():
+
+            index_key = ((ord(i) - ord('a')) - key) % 26 + ord('A')
+            old_letter = chr(index_key)
+            result += old_letter
+
+        elif i.islower():
+
+            index_shifted_back = ((ord(i) - ord('a')) - key) % 26 + ord('a')
+            old_letter = chr(index_shifted_back)
+            result += old_letter
+        elif i.isdigit():
+
+            old_number = (int(i) - key) % 10
+            result += str(old_number)
+        else:
+            result += i
+
+    return result
+
+
+def crack(text):
+    for i in range(26):
+        word_decrypted =  decrypt(text,i)
+        word = re.sub(r'[^A-Za-z]+','', word_decrypted)
+        if word.lower() in word_list or word in name_list:
+            return (word_decrypted)
--- tests/test_caesar_cipher.py
+++ tests/test_caesar_cipher.py
@@ -1,5 +1,12 @@
 from caesar_cipher.caesar_cipher import *
 
-
 def test_encrypt():
-    pass
+    assert encrypt('helloo',5) == 'mjqqtt'
+
+
+def test_decrypt():
+    assert decrypt('mjqqtt',5) == 'helloo'
+
+
+def test_crack():
+    assert crack('stuj65') == 'nope10'
